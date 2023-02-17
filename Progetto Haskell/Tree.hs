28ec3b41122a7a6b4929dfde3a804719d532b76a
LorenzoF6 <lorenzo05698@gmail.com>
2023-02-17 18:23:32 +0100

    Update Tree.hs

--- Progetto Haskell/Tree.hs
+++ Progetto Haskell/Tree.hs
@@ -1,10 +1,15 @@
 module Tree where
 
 -- Definizione del tipo di dato Albero
-data Tree a = Empty | Node a (Tree a) (Tree a)  deriving (Ord,Eq,Show)
+data Tree a = Empty | Node a (Tree a) (Tree a)  deriving (Eq,Show)
 
 --INSERIMENTO E RIMOZIONE DI ELEMENTI NELL'ALBERO
 --Inserisce un elemento nell'albero
+--Caso base: se si inserisce x nell'albero vuoto, si ottiene un albero che contiene solo x
+--Passo induttivo: 
+-- x == a : l'albero è invariato e come = si ha l'espressione che ricostruisce lo stesso albero
+-- x < a : elemento più piccolo della radice, si continua la ricerca per l'inserimento nel sotto-albero sinistro
+-- x > a : caso duale
 insert :: (Ord a) => a -> Tree a -> Tree a
 insert x Empty = Node x Empty Empty
 insert x (Node a left right)
@@ -13,19 +18,33 @@ insert x (Node a left right)
     | x >  a = Node a left (insert x right)
 
 --Rimuove un elemento presente nell'albero
+--Caso base: gestisce situazione di albero vuoto (1)
+--Casi di rimozione dell'elemento che più piccolo/grande di quello che si trova nella radice:
+--in questi casi la radice n soppravvive mentre x viene cercato e rimosso in uno dei due sotto-alberi
+--a seconda della sua relazione con la radice (2)
+--Casi in cui l'elemento da rimuovere si trova nella radice:
+--(3) gestisce i casi in cui uno dei due sotto-alberi è vuoto  
+--(5) gestisce il caso in cui nessun sotto-albero è vuoto:
+--in questo caso come nuova radice si sceglie l'elemnto più grande nell'sotto-albero sinistro
 delete :: Ord a => a -> Tree a -> Tree a
-delete _ Empty = Empty
-delete x (Node n left right)
+delete _ Empty = Empty --(1)
+delete x (Node n left right) --(2)
    | x < n = Node n (delete x left) right
    | x > n = Node n left (delete x right)
-delete x (Node _ t Empty) = t
-delete x (Node _ Empty t) = t
-delete x (Node _ left right)  = Node n (delete n left) right
+delete x (Node _ t Empty) = t --(3.1)
+delete x (Node _ Empty t) = t --(3.2)
+delete x (Node _ left right)  = Node n (delete n left) right --(5)
   where
     n = maxElem left
 
 --Genera un albero a partire da una List fornita in Input
 --Si assume che il primo elemento della lista sia la testa dell'albero
+--Caso base: lista vuota in input, crea un albero vuoto
+--Passo induttivo: 
+--per l'elemento x (la testa) della lista xs, genera un albero avente
+--Node l'elemento x
+--sotto-albero sinitro gli elementi <x presenti in xs (tramite funzione filter)
+--sotto-albero destro gli elementi >x presenti in xs (tramite funzione filter)
 treeFromList :: Ord a => [a] -> Tree a
 treeFromList [] = Empty
 treeFromList (x:xs) = Node x (treeFromList (filter (<x) xs)) (treeFromList (filter (>x) xs)) 
@@ -72,9 +91,12 @@ searchElem x (Node a left right)
     | x <  a = searchElem x left
     | x >  a = searchElem x right
 
+--Si sfrutta il tipo Maybe = Just | Nothing, il quale incapsula un valore facoltativo
+--Un valore di tipo Maybe a contiene una valore di tipo a, caso di Just a, oppure è vuoto, caso Nothing
+--Maybe gestisce casi eccezzionali, come in questo caso la gestione della ricerca di un elemento in un albero vuoto
 --Ricerca valore Max nell'albero
 maxElem :: Tree a -> Maybe a
-maxElem Empty = Nothing
+maxElem Empty = Nothing 
 maxElem (Node x _ Empty) = Just a
 maxElem (Node _ _ t)    = maxElem t
 
@@ -87,6 +109,11 @@ minElem (Node _ t _)    = minElem t
 
 --ALTRE OPERAZIONI
 --Verifica che un albero è un albero binario
+--Caso base: un albero vuoto è sempre binario
+--Passo induttivo
+--chiamata ricorsiva sui sotto-alberi sinistro e destro verificando che:
+--sotto-albero sinistro: sia vuoto oppure ogni x è minore dell'elemento massimo
+--sotto-albero destro: sia vuoto oppure ogni x è maggiore dell'elemento minore
 isBinaryTree :: Ord a => Tree a -> Bool
 isBinaryTree Empty = True
 isBinaryTree (Node x left right) = isBinaryTree left && isBinaryTree right 
@@ -94,6 +121,11 @@ isBinaryTree (Node x left right) = isBinaryTree left && isBinaryTree right
                                    && (isEmpty right || x < minElem right)
                                                          
 --Visualizzazione dell'albero
+--
+--
+--
+--
+--
 printTree :: Show a => Tree a -> IO ()
 printTree = (mapM_ putStrLn) . treeIndent
   where
@@ -102,6 +134,8 @@ printTree = (mapM_ putStrLn) . treeIndent
       where
         (r:rs) = treeIndent $ right
         l      = treeIndent $ left
+
+
 
 --TEST 
 --Main di prova per testare il modulo Tree.hs
